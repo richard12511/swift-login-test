03eed8763f927939d1817170968fc17dd5751e12
Richard Schmidt <richardnew12511@gmail.com>
2021-01-02 13:21:46 -0600

    first commit

---
+++ swift-login-test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-login-test2
+//
+//  Created by Richard Schmidt on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-login-test2/swift_login_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_login_test2App.swift
+//  swift-login-test2
+//
+//  Created by Richard Schmidt on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_login_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
